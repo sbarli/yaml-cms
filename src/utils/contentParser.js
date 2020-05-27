11d607b48ddfdcdf6e27de9480f168d585aba92a
Samantha Salley <samanthasalley@gmail.com>
2020-05-27 02:24:22 -0700

    (remove)  🔥  🧹  test console logs

--- src/utils/contentParser.js
+++ src/utils/contentParser.js
@@ -6,27 +6,20 @@ import Header from '../ui/Header/Header';
 import Codeblock from '../ui/Codeblock/Codeblock';
 
 export const generateContent = (rawContent, contentId) => {
-  console.log('rawContent: ', rawContent);
   if (typeof rawContent === 'string') {
-    console.log('raw content is string');
     return <Paragraph key={contentId} content={rawContent} />;
   }
   if (Array.isArray(rawContent)) {
-    console.log('raw content is array');
     return rawContent.map((item, contentIdx) => {
-      console.log('generating content for item: ', item);
       return generateContent(item, `${contentId}-${contentIdx}`);
     });
   }
-  console.log('raw content is object');
   return Object.keys(rawContent)
     .map((key, contentIdx) => {
       switch (key) {
         case contentKeyMap.PARAGRAPH:
-          console.log(`hit ${contentKeyMap.PARAGRAPH}`);
           return <Paragraph key={contentId} content={rawContent[key]} />;
         case contentKeyMap.CODEBLOCK:
-          console.log(`hit ${contentKeyMap.CODEBLOCK}`);
           return (
             <Codeblock
               key={contentId}
@@ -35,7 +28,6 @@ export const generateContent = (rawContent, contentId) => {
             />
           );
         case contentKeyMap.HEADER:
-          console.log(`hit ${contentKeyMap.HEADER}`);
           const headerData = rawContent[key];
           const HeaderContent =
             typeof headerData.content === 'string'
@@ -47,7 +39,6 @@ export const generateContent = (rawContent, contentId) => {
             </Header>
           );
         case contentKeyMap.NESTED_CONTENT:
-          console.log(`hit ${contentKeyMap.NESTED_CONTENT}`);
           return generateContent(rawContent[key], `${contentId}-content`);
         default:
           console.log(`hit default case for key ${key}`);
