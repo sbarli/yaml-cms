94669b4cc07eebf0a05e10eabc7a55e5e657a4ae
Samantha Salley <samanthasalley@gmail.com>
2020-05-27 04:22:51 -0700

    (add)  ❇️  logic to handle new component rendering via contentParser

--- src/utils/contentParser.js
+++ src/utils/contentParser.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { contentKeyMap } from './constants';
 
+import Topics from '../components/Topics';
 import Paragraph from '../components/ui/Paragraph/Paragraph';
 import Header from '../components/ui/Header/Header';
 import Codeblock from '../components/ui/Codeblock/Codeblock';
+import Tagline from '../components/ui/Tagline/Tagline';
+import OrderedList from '../components/ui/OrderedList/OrderedList';
+import UnorderedList from '../components/ui/UnorderedList/UnorderedList';
+import ListItem from '../components/ui/ListItem/ListItem';
 
 export const generateContent = (rawContent, contentId) => {
   if (typeof rawContent === 'string') {
@@ -19,12 +24,17 @@ export const generateContent = (rawContent, contentId) => {
       switch (key) {
         case contentKeyMap.PARAGRAPH:
           return <Paragraph key={contentId} content={rawContent[key]} />;
+        case contentKeyMap.TOPICS:
+          const TopicsList = generateContent(rawContent[key], `${contentId}-${key}`);
+          return <Topics key={`${contentId}-${key}`}>{TopicsList}</Topics>;
+        case contentKeyMap.TAGLINE:
+          return <Tagline key={contentId}>{rawContent[key]}</Tagline>;
         case contentKeyMap.CODEBLOCK:
           return (
             <Codeblock
-              key={contentId}
+              key={`${contentId}-${key}`}
               codeblock={rawContent[key]}
-              codeblockId={`${contentId}-codeblock`}
+              codeblockId={`${contentId}-${key}`}
             />
           );
         case contentKeyMap.HEADER:
@@ -32,14 +42,45 @@ export const generateContent = (rawContent, contentId) => {
           const HeaderContent =
             typeof headerData.content === 'string'
               ? headerData.content
-              : generateContent(headerData.content, `${contentId}-header`);
+              : generateContent(headerData.content, `${contentId}-${key}`);
           return (
-            <Header key={contentId} size={headerData.size}>
+            <Header key={`${contentId}-${key}`} size={headerData.size}>
               {HeaderContent}
             </Header>
           );
+        case contentKeyMap.ORDERED_LIST:
+          const OrderedListContent = generateContent(
+            rawContent[key],
+            `${contentId}-${key}`
+          );
+          return (
+            <OrderedList key={`${contentId}-${key}`}>
+              {OrderedListContent}
+            </OrderedList>
+          );
+        case contentKeyMap.UNORDERED_LIST:
+          const UnorderedListContent = generateContent(
+            rawContent[key],
+            `${contentId}-${key}`
+          );
+          return (
+            <UnorderedList key={`${contentId}-${key}`}>
+              {UnorderedListContent}
+            </UnorderedList>
+          );
+        case contentKeyMap.LIST_ITEM:
+          const ListItemContent =
+            typeof rawContent[key] === 'string'
+              ? rawContent[key]
+              : generateContent(rawContent[key], `${contentId}-${key}`);
+          return (
+            <ListItem
+              key={`${contentId}-${key}`}
+              content={ListItemContent}
+            ></ListItem>
+          );
         case contentKeyMap.NESTED_CONTENT:
-          return generateContent(rawContent[key], `${contentId}-content`);
+          return generateContent(rawContent[key], `${contentId}-${key}`);
         default:
           return null;
       }
